a264516292b63851e16f0dd788eadf69cbf6bbfd
Shabrina Hoque <hoqueshabrina@gmail.com>
2022-10-17 16:17:51 +0100

    solution

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,14 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    itemstr = map(str, items)
+    singleitems = set(map(str,itemstr))
+    
     frequencies = {}
+    for i in singleitems:
+        
+        frequencies[i] = itemstr.count(i)    
+    
     # Your code goes here
     return frequencies
+
