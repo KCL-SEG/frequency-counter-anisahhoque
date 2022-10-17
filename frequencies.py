84489b7a6c72d1b5bc0205fd54253d389dcf446c
anisahhoque <68054548+anisahhoque@users.noreply.github.com>
2022-10-17 02:33:25 +0100

    Add files via upload

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,15 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    itemstr = map(str, items)
+    singleitems = set(map(str,itemstr))
+    
     frequencies = {}
+    for i in singleitems:
+        
+        frequencies[i] = itemstr.count(i)    
+    
     # Your code goes here
+    print(frequencies)
     return frequencies
+
